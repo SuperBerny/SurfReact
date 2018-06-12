220131202cf97073e04836c2428168b3c0ecea9a
Brian Patrick <brianp@hjimail.com>
2018-06-12 11:42:57 -0700

    gitignore stuff for api key, probably not the safest way, but whatever

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+
 
 class SearchBox extends Component {
+
+   state = {
+      data: null
+   }
+
+   componentDidMount() {
+      axios.get('')
+   }
+
    render() {
       return(
-         <div className="searchbox">
-               <input type="text" placeholder="Search Netflix Shows" />
+         <div className="main">
+            <div className="searchbox">
+                  <input type="text" placeholder="Search Netflix Shows" />
+            </div>
+            <div className="api-dump">
+
+            </div>
          </div>
       )
    }
