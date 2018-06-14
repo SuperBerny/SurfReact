e7e27c0aef538b95311169132adaf2264fd36fe8
Brian Patrick <brianp@hjimail.com>
2018-06-14 13:02:31 -0700

    trying to get my loops to work

--- leafletmap.js
+++ leafletmap.js
@@ -1,4 +1,26 @@
-var mymap = L.map('mapid').setView([33.0338239,-117.2907376], 12);
+let url = 'http://api.spitcast.com/api/spot/all';
+
+$.ajax({
+    url: url,
+    method: "GET"
+})
+.done(function(response) {
+console.log(response[0].spot_name);
+const coordsArr = [];
+
+for( let spot in response ) {
+    // console.log('I\'m a spot');
+    // coordsArr.push([spot.spot_name, spot.latitude, spot.longitude])
+    // console.log(response);
+};
+
+console.log('_____');
+console.log(coordsArr);
+
+
+// Create leaflet map instance
+
+var mymap = L.map('mapid').setView([33.0338239,-117.2907376], 15);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -6,6 +28,16 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     id: 'mapbox.streets',
     accessToken: 'pk.eyJ1IjoiYnJpYW5wYXRyaWNrIiwiYSI6ImNqaWV1ZXZ5ajBndjM0MW9jdDFseW0wMnIifQ.SZfb6_fwjzTpA1bSegppJg'
 }).addTo(mymap);
+
+
+//Add map markers from coordsArr from ajax call to Spitcast
+var marker = L.marker([33.033239, -117.2907376]).addTo(mymap);
+
+
+// ajax call closer
+}) // ajax call closer
+//ajax call closer
+
 
 
 const submit = document.querySelector('.coordSearch');
@@ -35,4 +67,3 @@ submit.addEventListener('click', function(e) {
     console.log(coordsArr);
 
 });
-
